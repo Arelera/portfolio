d3a54f7a4b17723106afc2f4df33bfd1fa66da3d
Arel <arelcode@gmail.com>
2021-03-13 21:18:53 +0300

    Added Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,15 @@ import Footer from '../components/Footer/Footer';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
+  const addGA = () => {
+    if (typeof window == 'undefined') return;
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-JN233J1JKH');
+  };
   return (
     <>
       <Head>
@@ -11,6 +20,11 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Open+Sans:wght@300;400&display=swap"
           rel="stylesheet"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JN233J1JKH"
+        ></script>
+        <script>{addGA()}</script>
       </Head>
       <Component {...pageProps} />
       <Footer />
